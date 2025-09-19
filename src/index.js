b3a6893e12f633b1b2adf432f214f9d9efde7d8b
bkoimett <benkoimet@gmail.com>
2025-09-19 11:47:04 +0300

    feat: implement signup page with form structure and styling; update CSS for button colors

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ app.use(express.static('public'));
 app.get('/', (req,res) => {
     res.render('login');
 })
+app.get('/login', (req,res) => {
+    res.render('login');
+}) 
 
 app.get('/signup', (req,res) => {
     res.render('signup');
