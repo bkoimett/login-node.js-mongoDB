6ba915432fe36da4708db06ff5048d423775deec
bkoimett <benkoimet@gmail.com>
2025-09-19 13:33:08 +0300

    feat: implement login functionality with user validation and error handling; add home page view

--- src/index.js
+++ src/index.js
@@ -72,6 +72,27 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Login user 
+app.post("/login", async (req, res) => {
+    try {
+        const check = await auth.findOne({ name: req.body.username });
+        if (!check) {
+            res.send("User name cannot found")
+        }
+        // Compare the hashed password from the database with the plaintext password
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (!isPasswordMatch) {
+            res.send("wrong Password");
+        }
+        else {
+            res.render("home");
+        }
+    }
+    catch {
+        res.send("wrong Details");
+    }
+});
+
 // Start the server
 // const port = 5000
 // app.listen(port, () => {
