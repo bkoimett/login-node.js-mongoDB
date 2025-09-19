416fd258c23ca1274394ff35bf5c97dbb6c60cb2
bkoimett <benkoimet@gmail.com>
2025-09-19 11:35:51 +0300

    feat: add login and signup views with basic form structure; implement CSS for styling

--- src/index.js
+++ src/index.js
@@ -6,20 +6,23 @@ const bcrypt = require('bcrypt');
 const app = express();
 
 // use EJS as view engine
-app.use('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('public'));
 
 // route handlers - define route
 app.get('/', (req,res) => {
-    res.render('login')
+    res.render('login');
 })
 
 app.get('/signup', (req,res) => {
-    res.render('signup')
+    res.render('signup');
 })
 
 
 // Start the server 
 const port = 5000
 app.listen(port, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on http://localhost:3000');
 })
